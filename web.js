345155693025e68847d1dd6320756673cdf62074
Andrey Belov <mayweed@gmail.com>
2013-07-08 00:58:44 +0400

    web.js 3

--- web.js
+++ web.js
@@ -6,11 +6,14 @@ var fs = require('fs');
 
 var helloText = fs.readFileSync("index.html");
 
+var bufferString = new Buffer(helloText, 'utf8');
+
 app.get('/', function(request, response) {
-  response.send(helloText);
+  response.send(bufferString.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
