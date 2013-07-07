c489b670f189e06488955adf4e1bee3046bafbfa
Andrey Belov <mayweed@gmail.com>
2013-07-08 00:31:07 +0400

    web.js 2

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger());  
+
+var fs = require('fs');
+
+var helloText = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(helloText);
 });
 
 var port = process.env.PORT || 5000;
